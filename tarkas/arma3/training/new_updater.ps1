ff86847cfd9bcfdc89276a3688b0ed0cba68b535
uaon <7cav.sweetwater@gmail.com>
2020-03-18 15:22:59 -0500

    local mod support
    ability to pull local mods from select directory

--- tarkas/arma3/training/new_updater.ps1
+++ tarkas/arma3/training/new_updater.ps1
@@ -5,15 +5,21 @@ $configJson = Get-Content -Raw -Path $config | ConvertFrom-Json
 $instanceId = $configJson.server.env.SERVER_ID
 $serverName = $configJson.server.env.SERVER_NAME
 $modListJson = $configJson.mods
+$serverModListJson = $configJson.servermods
+$localModListJson = $configJson.localmods
 $configDir = $configJson.server.env.ARMA_CONFIG_PATH
 $steamCMDdir = $configJson.server.env.STEAM_BASE_PATH
 $installDirWorkshop = $configJson.server.env.STEAM_WORKSHOP_PATH
 $installDir = $configJson.server.env.ARMA_BASE_PATH
 $installDirArmadirectory = $installDir
+$localModDir = $configJson.server.env.LOCAL_MOD_PATH
 $steamUser = $configJson.server.env.STEAM_USERNAME
 $steamPass = $configJson.server.env.STEAM_PASSWORD
 #
 $configDir = "$configDir\$serverName"
+
+#For purposes of updating no need to have seperate arrays for mods.
+$modListJson = $modListJson += $serverModListJson
 
 Write-Output "Update has started: $(Get-Date)"
 
@@ -47,6 +53,20 @@ foreach($item in $modListJson)
 
    New-Item -Path $installDirArmadirectory\$name -ItemType SymbolicLink -Value $installDirWorkshop\$id
 
+}
+
+#local mod retreival
+foreach($item in $localModListJson)
+{
+   $name = $item.name
+   $path = $localModDir
+
+   copy-item $path\$name $installDirArmadirectory\ -force -recurse
+   Write-Output copy-item $path\$name $installDirArmadirectory -force -recurse
+   copy-item $installDirArmadirectory\$name\keys\*.bikey $installDirArmadirectory\keys\ -force -recurse
+   Write-Output copy-item $installDirArmadirectory\$name\keys\*.bikey $installDirArmadirectory\keys\ -force -recurse
+   copy-item $installDirArmadirectory\$name\key\*.bikey $installDirArmadirectory\key\ -force -recurse
+   Write-Output copy-item $installDirArmadirectory\$name\key\*.bikey $installDirArmadirectory\key\ -force -recurse
 }
 
 #Removing logs after update
