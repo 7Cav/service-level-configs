4a43f6f3815a0be88e1d7fa5d119005b62d15fee
uaon <7cav.sweetwater@gmail.com>
2020-03-18 16:10:09 -0500

    init commit for handle unlock batch
    tool for unlocking files, requires installation of handle.exe

---
+++ utilities/handle_unlock.bat
@@ -0,0 +1,26 @@
+@echo off
+Setlocal
+REM ~~~~~~~~~~~~~~~~~~ HANDLE UNLOCK ~~~~~~~~~~~~~~~~~~
+REM |     Author: Sweetwater.I
+REM |     Created on March 18, 2020
+REM |     7thCavalry.us
+REM ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+ECHO --------------------------------------------------
+ECHO          UNLOCKING .PBO Files                    
+ECHO --------------------------------------------------
+ECHO          Directory: "%~dp0"
+ECHO --------------------------------------------------
+
+set EXT=pbo
+set HANDLE_PATH=D:\utils\Handle
+
+
+	for /f "eol=: delims=" %%F in ('dir /b /o-d "%~dp0\*.%EXT%"') do (
+	cls
+	echo "%~dp0\%%F"
+
+	for /F "tokens=3,6 delims=: " %%I IN ('"%HANDLE_PATH%\handle.exe %%F"') DO "%HANDLE_PATH%\handle.exe" -c %%J -y -p %%I
+	)
+)
+exit
