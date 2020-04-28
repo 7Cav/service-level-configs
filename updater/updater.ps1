61e91a4d58faf465cc4ab5a75f22f734f7827a38
Josh E <7cav.sweetwater@gmail.com>
2020-04-28 12:18:28 -0500

    formatting

--- updater/updater.ps1
+++ updater/updater.ps1
@@ -1,11 +1,11 @@
 param (
-    [Parameter(Mandatory=$true)][string]$Server,
-    [Parameter(Mandatory=$true)][string]$Game,
-    [Parameter(Mandatory=$true)][string]$Type,
-    [Parameter(Mandatory=$true)][string]$Config,
-    [switch]$AutoStart = $false,
-    [switch]$logs = $false,
-    [switch]$Nuke = $false 
+  [Parameter(Mandatory = $true)][string]$Server,
+  [Parameter(Mandatory = $true)][string]$Game,
+  [Parameter(Mandatory = $true)][string]$Type,
+  [Parameter(Mandatory = $true)][string]$Config,
+  [switch]$AutoStart = $false,
+  [switch]$logs = $false,
+  [switch]$Nuke = $false 
 )
 $Server += '/'
 $Game += '/'
@@ -19,10 +19,12 @@ Set-Location -Path $locpath
 # 
 if ($Game -eq "arma3/") {
   .\main\arma3.ps1 $configPath $AutoStart $logs $Nuke
-} elseif ($Game -eq "squad/") {
+}
+elseif ($Game -eq "squad/") {
   .\main\arma3.ps1 $configPath $AutoStart $logs $Nuke
-} else {
-    throw "Game Type not Found"
+}
+else {
+  throw "Game Type not Found"
 }
 
 
