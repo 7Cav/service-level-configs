594fa5be83ac421c40b304123cbe6448a37b8e5e
Josh E <7cav.sweetwater@gmail.com>
2020-05-31 17:39:39 -0500

    config building test
    testing config building

--- ts-updater/src/index.ts
+++ ts-updater/src/index.ts
@@ -1,1 +1,20 @@
-console.log("This is where the magic happens!");
+const fs = require("fs");
+
+var example = "=7cav=";
+var path = require("path");
+
+function renderConfig(
+  templatePath: string,
+  object: { [x: string]: any; hostname?: string }
+) {
+  const template = fs.readFileSync(
+    path.join(__dirname, templatePath + ".cfg"),
+    "utf8"
+  );
+  return template.match(/\{{(.*?)\}}/gi).reduce((acc: any, binding: string) => {
+    const property = binding.substring(2, binding.length - 2);
+    return `${acc}${template.replace(/\{{(.*?)\}}/, object[property])}`;
+  }, "");
+}
+
+console.log(renderConfig("server", { hostname: example }));
