80380c0c4db27c59bad4af999417007b7f5de2d7
Josh E <7cav.sweetwater@gmail.com>
2020-05-31 21:18:53 -0500

    cultivating mass
    all the data into one json, isn't it glorious

--- ts-updater/src/index.ts
+++ ts-updater/src/index.ts
@@ -1,20 +1,35 @@
-const fs = require("fs");
-
-var example = "=7cav=";
+var fs = require("fs");
 var path = require("path");
+var Mustache = require("mustache");
+var fetch = require("node-fetch");
 
-function renderConfig(
-  templatePath: string,
-  object: { [x: string]: any; hostname?: string }
-) {
-  const template = fs.readFileSync(
-    path.join(__dirname, templatePath + ".cfg"),
-    "utf8"
+// Get external data with fetch
+const data = fetch("data.json").then((response) => {
+  return response.json();
+});
+
+// Get external template with fetch
+const template = fetch("server.cfg").then((response) => {
+  return response.text();
+});
+
+// wait for all the data to be received
+Promise.all([data, template])
+  .then((response) => {
+    const resolvedData = response[0];
+    const resolvedTemplate = response[1];
+
+    // Cache the template for future uses
+    Mustache.parse(resolvedTemplate);
+
+    var output = Mustache.render(resolvedTemplate, resolvedData);
+
+    // Write out the rendered template
+    return fs.writeFile("server.cfg", output, (err: any) => {
+      if (err) throw err;
+      console.log("generated template");
+    });
+  })
+  .catch((error) =>
+    console.log("Unable to get all template data: ", error.message)
   );
-  return template.match(/\{{(.*?)\}}/gi).reduce((acc: any, binding: string) => {
-    const property = binding.substring(2, binding.length - 2);
-    return `${acc}${template.replace(/\{{(.*?)\}}/, object[property])}`;
-  }, "");
-}
-
-console.log(renderConfig("server", { hostname: example }));
