4352d3878a762d57197c14009dd418984e337a10
Pingoo <>
2023-01-05 21:52:02 -0500

    Add DCS server files and penalty history.

---
+++ dcs/fox3ms/private/server-slmod-backups/BannedClients.lua
@@ -0,0 +1,16 @@
+slmod_banned_ips = 
+{
+    ["65.94.120.110"] = "JazzHands",
+    ["96.248.98.48"] = "AMERICA 6-9",
+    ["108.24.138.101"] = "Xbox360",
+} -- end of slmod_banned_ips
+
+
+slmod_banned_ucids = 
+{
+    ["1f2374b777f5faa252785977e9e612df"] = "JazzHands",
+    ["80360a1cbaaad5ec927ecc9c0a2df4cf"] = "AMERICA 6-9",
+    ["69e6e7490e5ea5a7895f78f25e4a14f3"] = "Xbox360",
+} -- end of slmod_banned_ucids
+
+-- TODO: Add player "Nuke 1-1 | Trumdum" to banned ips + ucids.
