e11f47665a2c641eb05c089c70de6199e7791f3a
SyniRon <66834451+SyniRon@users.noreply.github.com>
2023-03-08 05:54:11 -0500

    Refactoring a3update.py

--- pterodactyl/deployments/a3update/a3update.py
+++ pterodactyl/deployments/a3update/a3update.py
@@ -23,105 +23,179 @@
 # SOFTWARE.
 
 import contextlib
+import json
 import os
 import os.path
+import pathlib
+import queue
 import re
 import shutil
-import time
-import requests
-import json
 import threading
-
-from glob import glob
+import time
 from datetime import datetime
-from urllib import request
+from glob import glob
+from pathlib import Path
 
+import requests
 
 thread_local = threading.local()
 
 # region Configuration / ENV
+
+# Set variables for SteamCMD, Steam user, password, and GitHub mods URL
 STEAM_CMD = "/home/container/steamcmd/steamcmd.sh"  # Alternatively "steamcmd" if package is installed
 STEAM_USER = os.getenv("STEAM_USER")
 STEAM_PASS = os.getenv("STEAM_PASS")
-GITHUB_MODS = os.getenv("GITHUB_MODS_URL")
+GITHUB_MODS_URL = os.getenv("GITHUB_MODS_URL")
 
+# Set variables for Arma 3 server ID, server directory, and workshop ID
 A3_SERVER_ID = "233780"
 A3_SERVER_DIR = "/home/container"
 A3_WORKSHOP_ID = "107410"
 
+# Set variables for Arma 3 workshop and mods directories, and keys directory
 A3_WORKSHOP_DIR = f"{A3_SERVER_DIR}/steamapps/workshop/content/{A3_WORKSHOP_ID}"
 A3_MODS_DIR = "/home/container"
 A3_KEYS_DIR = "/home/container/keys"
 
-# MODPACK_NAME = "Modpack"
+# Set variable for modpack path
 MODPACK_PATH = "/home/container/modpack.html"
 
+# Set regular expressions for update pattern and title pattern
 UPDATE_PATTERN = re.compile(r"workshopAnnouncement.*?<p id=\"(\d+)\">", re.DOTALL)
 TITLE_PATTERN = re.compile(
     r"(?<=<div class=\"workshopItemTitle\">)(.*?)(?=<\/div>)", re.DOTALL
 )
+
+# Set variable for workshop changelog URL
 WORKSHOP_CHANGELOG_URL = "https://steamcommunity.com/sharedfiles/filedetails/changelog"
 
+# Remove cache lock file if it exists
 with contextlib.suppress(Exception):
     os.remove("/home/container/serverprofile/DataCache/cache_lock")
 
+# endregion
 
-# Thread Testing:
+# Set up a thread-local session for multi-threading support
+thread_local = threading.local()
+
+
 def get_session():
+    """
+    Returns a requests.Session object for thread testing.
+
+    This function creates a new requests.Session object and attaches it to the
+    current thread, using a thread-local variable. If a session object has
+    already been created for the current thread, it returns that object instead
+    of creating a new one.
+
+    Returns:
+        A requests.Session object.
+    """
     if not hasattr(thread_local, "session"):
         thread_local.session = requests.Session()
     return thread_local.session
 
 
-# endregion
-# Grab json from github
-response = requests.get(GITHUB_MODS)
+# Send a GET request to the GitHub mods URL and parse the response as JSON
+response = requests.get(GITHUB_MODS_URL)
 jsdata = json.loads(response.text)
-# MODPACK_NAME = jsdata.get('modpack_name')
-MODS = jsdata.get("mods")
-# SERVER_MODS = jsdata.get('server_mods')
-# OPTIONAL_MODS = jsdata.get('optional_mods')
-# DLC = jsdata.get('dlc')
 
-# end json grab
+# Extract the 'mods' list from the JSON data
+MODS = jsdata.get("mods")
+
 
 # region Functions
 
 
 def log(msg):
-    print("")
-    print("{{0:=<{}}}".format(len(msg)).format(""))
-    print(msg)
-    print("{{0:=<{}}}".format(len(msg)).format(""))
+    """
+    Prints a message with a header and footer of equal length made of equal signs.
+
+    Args:
+        msg (str): The message to be logged.
+    """
+    header_footer = "=" * len(msg)  # Create a header and footer of equal length
+    print(
+        f"\n{header_footer}\n{msg}\n{header_footer}\n"
+    )  # Print the header, message, and footer
 
 
 def call_steamcmd(params):
-    os.system(f"{STEAM_CMD} {params}")
-    print("")
+    """
+    Calls the SteamCMD executable with the specified parameters.
+
+    Args:
+        params (str): The parameters to be passed to SteamCMD.
+    """
+    os.system(
+        f"{STEAM_CMD} {params}"
+    )  # Execute the SteamCMD command with the specified parameters
+    print("\n")  # Print a newline character after the command has completed
 
 
 def update_server():
-    steam_cmd_params = (
-        f" +force_install_dir {A3_SERVER_DIR}"
-        + f" +login {STEAM_USER} {STEAM_PASS}"
-        + f" +app_update {A3_SERVER_ID} validate"
-        + " +quit"
-    )
-    call_steamcmd(steam_cmd_params)
+    """
+    Updates the ArmA 3 server using SteamCMD.
+    """
+    # Create a list of SteamCMD parameters for updating the server
+    steam_cmd_params = [
+        "+force_install_dir",
+        A3_SERVER_DIR,
+        "+login",
+        STEAM_USER,
+        STEAM_PASS,
+        "+app_update",
+        A3_SERVER_ID,
+        "validate",
+        "+quit",
+    ]
+
+    # Join the list of parameters into a single string separated by spaces
+    steam_cmd_str = " ".join(steam_cmd_params)
+
+    call_steamcmd(steam_cmd_str)  # Call SteamCMD with the specified parameters
 
 
-def mod_needs_update(mod_id, path):
-    if os.path.isdir(path):
-        response = request.urlopen(f"{WORKSHOP_CHANGELOG_URL}/{mod_id}").read()
-        response = response.decode("utf-8")
-        if match := UPDATE_PATTERN.search(response):
-            updated_at = datetime.fromtimestamp(int(match.group(1)))
-            created_at = datetime.fromtimestamp(os.path.getmtime(path))
-            print("[DEBUG] Workshop date", datetime.fromtimestamp(int(match.group(1))))
-            print(
-                "[DEBUG] Modified date", datetime.fromtimestamp(os.path.getmtime(path))
-            )
-            return updated_at >= created_at
+def is_mod_outdated(mod_id: int, path: str) -> bool:
+    """
+    Checks if a mod needs an update by comparing its modified date with its last updated date in the workshop.
+
+    Args:
+        mod_id (int): The ID of the mod to check.
+        path (str): The path to the mod directory.
+
+    Returns:
+        bool: True if the mod needs an update, False otherwise.
+    """
+    mod_path = Path(path)
+
+    # Check if the path exists
+    if not mod_path.exists():
+        print(f"[WARNING] Directory {path} does not exist")
+        return True
+
+    # Fetch the URL and get the match object if it exists
+    try:
+        response = requests.get(f"{WORKSHOP_CHANGELOG_URL}/{mod_id}")
+        response.raise_for_status()
+        match = UPDATE_PATTERN.search(response.text)
+    except requests.exceptions.RequestException as e:
+        print(f"[ERROR] An error occurred: {e}")
+        return False
+
+    if mod_path.is_dir() and match:
+        created_at = datetime.fromtimestamp(mod_path.stat().st_mtime)
+        updated_at = datetime.fromtimestamp(int(match.group(1)))
+        # Debugging information
+        print("[DEBUG] Workshop date", updated_at)
+        print("[DEBUG] Modified date", created_at)
+        return updated_at >= created_at
+    else:
+        print(f"[WARNING] {path} is not a directory")
+        return False
+
+
 
     return False
 
@@ -265,28 +339,20 @@ def save_rpt():
 # endregion
 
 
-log("Backing up RPT")
-save_rpt()
-
-log(f"Updating A3 server ({A3_SERVER_ID})")
-
-update_server()
-
-log("Updating mods")
-
-try:
-    update_mods()
-except Exception:
-    print("error on mod update!!")
-
-log("Converting uppercase files/folders to lowercase...")
-lowercase_workshop_dir()
-
-log("Creating symlinks...")
-create_mod_symlinks()
-
-
-log("Copying server keys...")
-copy_keys()
-
-log("Done!")
+if __name__ == "__main__":
+    log("Backing up RPT")
+    save_rpt()
+    log(f"Updating A3 server ({A3_SERVER_ID})")
+    # update_server()
+    log("Updating mods")
+    try:
+        update_mods()
+    except Exception:
+        print("error on mod update!!")
+    log("Converting uppercase files/folders to lowercase...")
+    lowercase_workshop_dir()
+    log("Creating symlinks...")
+    create_mod_symlinks()
+    log("Copying server keys...")
+    copy_keys()
+    log("Done!")
